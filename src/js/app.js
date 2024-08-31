2242ef7e3ce04af1e873c9d505bd4bd421f0656c
DavidKimDY <kdaeyoub@gmail.com>
2024-08-31 16:50:54 +0900

    boiler plate 임

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+class App{
+    constructor(){
+        this.canvas = document.getElementById('canvas');
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
+
+        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
+    }
+
+    resize(){
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
+
+        this.canvas.width = this.stageWidth * this.pixelRatio;
+        this.canvas.height = this.stageHeight * this.pixelRatio;
+        this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+    }
+    
+}
